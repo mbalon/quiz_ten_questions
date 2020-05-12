f65a8398553b800c9a020acee13fc66e9b00110c
Mateusz Balon <balonmateusz@gmail.com>
2020-05-12 13:58:20 +0200

    Add functions: get_user_question, create, convert_to_json

---
+++ questions.py
@@ -0,0 +1,46 @@
+import uuid
+import json
+
+
+class Questions:
+
+    def __init__(self, content='', good_ans='', wrong_ans=['']):
+        self.id = None
+        self.content = content
+        self.good_ans = good_ans
+        self.wrong_ans = wrong_ans
+
+        self.question = {self.content: [self.good_ans]+self.wrong_ans}
+
+    @staticmethod
+    def get_user_question():
+        print('''
+                    Input a new question in following way:
+                    Question;goodAnswer;wrongAnswer;wrongAnswer;wrongAnswer
+                ''')
+        new_record = input().split(';')
+        return new_record
+
+    def create(self, new_record):
+        self.id = uuid.uuid4()
+        self.content = new_record[0]
+        self.good_ans = new_record[1]
+        self.wrong_ans = new_record[2:]
+        self.question = {self.content: [self.good_ans]+self.wrong_ans}
+
+        return self.question
+
+    def convert_to_json(self):
+        str = ''' "id": {} "content": {} "good_ans": {} "wrong_ans": {}'''\
+            .format(self.id, self.content, self.good_ans, self.wrong_ans)
+        json_str = json.dumps(str)
+        return json_str
+
+    def save_question_to_file(self):
+        pass
+
+    def load(self):
+        pass
+
+    def display(self):
+        pass
